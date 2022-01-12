92cf9cdbc1eda5a9ec91f4cb30749e6810292c7e
aysoodaakbari <aysoodaakbariii5515@gmail.com>
2022-01-12 23:06:52 +0330

    building front base

---
+++ my-potify/frontend-potify/src/App.js
@@ -0,0 +1,22 @@
+
+import "./App.css";
+
+import { Leftmenu } from './components/leftmenu';
+import { MainContainer } from './components/MainContainer';
+import { Rmenu } from './components/Rmenu';
+
+function App() {
+    return ( 
+         <div className="App">
+      <div className=""></div>
+<Leftmenu/>
+<MainContainer/>
+<Rmenu/>
+<div className='background'>
+
+</div>
+        </div>
+    );
+}
+
+export default App;
