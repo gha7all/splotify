141078ce93f67d48d542334eda26b34dc5356867
Gha7all <gha7alkh3yr0ddin@gmail.com>
2022-01-13 22:38:51 +0330

    adds api configurations

---
+++ config.py
@@ -0,0 +1,7 @@
+user = 'put your spotify username'
+client_id = 'put the client id in here'
+client_secret = 'put the client_secret in here'
+redirect_uri = 'http://localhost:7777/callback' # don't forget to set 
+                                                # this up on the dashboard
+                                                # of developers.spotify
+scope = 'user-read-recently-played' 
