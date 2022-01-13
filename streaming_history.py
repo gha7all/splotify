1ee1c40660ae6b160c0a9ba3c1c4155b38f27c07
Gha7all <gha7alkh3yr0ddin@gmail.com>
2022-01-13 22:39:25 +0330

    gets data from api

--- streaming_history.py
+++ streaming_history.py
@@ -4,18 +4,34 @@ from requests.api import head
 import spotipy
 import pandas as pd
 import spotipy.util as util
-import ast
+import json
+
+user = '46jsxjwf7z2hvkpvm6mkag69n'
+client_id = 'bf7aad0983e348b7b6a1c5d98514af95'
+client_secret = '42ea3d0d007d405aa73b8312658f2aa5'
+redirect_uri = 'http://localhost:7777/callback'
+scope = 'user-read-recently-played'
+path = 'plotify/dataset/StreamingHistory0.json'
 
 
-def get_token(user, client_id, client_secret, redirect_url, scope):
+def get_token(user, client_id, client_secret, redirect_uri, scope):
 
     token = util.prompt_for_user_token(
-        user, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_url)
+        user, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
 
     return token
 
 
-def api_id(track_info, token, artist):
+def get_streamings(path):
+    with open(path, 'r', encoding='UTF-8') as json_file:
+        dict_json = json.load(json_file)
+    history = pd.DataFrame.from_dict(dict_json, orient='columns')
+    history.reset_index(level=0, inplace=True)
+
+    return history
+
+
+def get_id(track_info, token, artist):
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
@@ -29,17 +45,63 @@ def api_id(track_info, token, artist):
 
     try:
         response = requests.get(
-            'https://api.spotify.com/v1/search', headers=headers, timeout=5)
+            'https://api.spotify.com/v1/search', headers=headers, params=params, timeout=5)
         json = response.json()
-        results = json['tracks']['items']
         first_result = json['tracks']['items'][0]
-        # check whether the found artist is the right one
+        results = json['tracks']['items']
+
         if artist:
             for result in results:
                 if artist.strip() == result['artists'][0]['name'].strip():
                     track_id = result['id']
         track_id = first_result['id']
-        return track_id
 
+        return track_id
     except:
         return None
+
+
+def get_features(track_id, token):
+    sp = spotipy.Spotify(auth=token)
+    try:
+        features = sp.audio_features([track_id])
+        return features[0]
+    except:
+        return None
+
+
+def append_features(token):
+    streamings = get_streamings(path)
+    unique_tracks = streamings.drop_duplicates(
+        subset=['trackName', 'artistName'], keep='first')
+
+    all_features = {}
+    for track in unique_tracks['trackName']:
+        artist = unique_tracks.loc[unique_tracks['trackName']
+                                   == track, 'artistName'].values[0]
+        track_id = get_id(track, token, artist)
+        features = get_features(track_id, token)
+        if features:
+            all_features[track] = features
+
+    final_features = []
+    for track_name, features in all_features.items():
+        final_features.append({'name': track_name, **features})
+
+    history_features = pd.DataFrame(final_features)
+    return history_features
+
+
+token = get_token(user=user, client_id=client_id,
+                  client_secret=client_secret, redirect_uri=redirect_uri, scope=scope)
+
+
+# all commented lines below this are working just fine. 
+# jt_mirrors = get_id('Mirrors', token, artist='Justin Timberlake')
+# mirrors_features = get_features(jt_mirrors, token)
+
+# print('token    ', token)
+# print('id   ', jt_mirrors)
+# print('features', mirrors_features)
+# df = append_features(token)
+# df.to_csv('streaming_audio_features.csv')
