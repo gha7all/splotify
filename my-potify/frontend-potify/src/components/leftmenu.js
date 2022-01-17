a2c62f86c5f7d7bc8b2fde3f8daa04557ab1d4c8
aysoodaakbari <aysoodaakbariii5515@gmail.com>
2022-01-17 22:36:14 +0330

    searchbox

--- my-potify/frontend-potify/src/components/leftmenu.js
+++ my-potify/frontend-potify/src/components/leftmenu.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { FaSearch ,FaMusic} from 'react-icons/fa';
+import { FaSearch ,FaMusic,FaEllipsisH} from 'react-icons/fa';
+import "../styles/leftmenu.css"
 
 
 function Leftmenu() {
     return(
-        <div>
+        <div className='leftmenu'>
             <div className='logo-container'>
-            <h2><FaMusic/></h2></div>
+                <i><FaMusic/></i>
+            <h2>plotify</h2>
+        <i><FaEllipsisH/></i>
+            </div>
+            
+            <div className='searchbox'>
+                <input type='text' placeholder='search...'/>
+                <i className='searchicon'>< FaSearch/></i>
+            </div>
             </div>
             );
 }
