09c9e3f602edd0343972a30cccf8d7dc0f9f2ccc
aysoodaakbari <aysoodaakbariii5515@gmail.com>
2022-01-17 10:49:49 +0330

    add ract icons

--- my-potify/frontend-potify/src/components/leftmenu.js
+++ my-potify/frontend-potify/src/components/leftmenu.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { FaSearch ,FaMusic} from 'react-icons/fa';
 
 
 function Leftmenu() {
     return(
-        <div></div>
+        <div>
+            <div className='logo-container'>
+            <h2><FaMusic/></h2></div>
+            </div>
             );
 }
 
