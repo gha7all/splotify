8c319e4f6d8f3dcfce3f6d156ebf5da63d457a47
aysoodaakbari <aysoodaakbariii5515@gmail.com>
2022-01-19 21:35:23 +0330

    ...

--- my-potify/frontend-potify/src/components/menu.js
+++ my-potify/frontend-potify/src/components/menu.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 
 
 function Menu({title , menuObject}) {
+    useEffect(()=>
+    {
+        const allLi=document.querySelector(".menuContainer ul").querySelectorAll("li");
+        function changeMenuActive(){
+            allLi.forEach((n)=>n.classList.remove('active'));
+            this.classList.add("active");
+        }
+       allLi.forEach((n)=>n.addEventListener('click',changeMenuActive))
+    },[])
     return (
         <div className='menuContainer'>
             <p className='title'>{title}</p>
