9b3c74e84bf65022ae430ea457b3ab44f1072dbd
aysoodaakbari <aysoodaakbariii5515@gmail.com>
2022-02-13 22:54:06 +0330

    right menu

--- my-plotify/frontend-potify/src/components/MainContainer.js
+++ my-plotify/frontend-potify/src/components/MainContainer.js
@@ -3,14 +3,14 @@ import React from 'react'
 import "../styles/Maincontainer.css";
 // import { FaUsers } from "react-icons/fa";
 // import { AudioList } from "./AudioList";
-import { Banner } from "./Banner";
+//import { Banner } from "./Banner";
 
 import '../styles/Maincontainer.css'
 
 function MainContainer() {
     return(
         <div className='mainContainer'>
-             <Banner />
+      {/* write your code here */}
 
         </div>
             );
--- my-plotify/frontend-potify/src/components/Rmenu.js
+++ my-plotify/frontend-potify/src/components/Rmenu.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import '../styles/Rmenu.css'
-
+import { FaCrown, FaBell, FaRegHeart, FaSun, FaCogs } from "react-icons/fa";
+import Profile from "../img/profile.jpg";
 
 function Rmenu() {
     return(
-<div className='rmenu'></div>
+        <div className="rightContainer">
+        <div className="goPro">
+          <i>
+            <FaCrown />
+            <p>
+              <span> Go</span>Pro
+            </p>
+          </i>
+  
+          <i>
+            <FaBell />
+          </i>
+  
+          <i>
+            <FaRegHeart />
+          </i>
+        </div>
+        <div className="profile">
+          <i>
+            <FaSun />
+          </i>
+          <i>
+            <FaCogs />
+          </i>
+  
+          <div className="profileImage">
+            <img src={Profile} alt="" />
+          </div>
+        </div>
+      </div>
     );
+    
 }
 
 export { Rmenu};
